515bf34ebbc5da0a608999403d1b7a66672d4bb5
Ganja Vasiliy <vasllly219@gmail.com>
2019-04-04 23:04:45 +0300

    FIX

--- src/js/ArrayBufferConverter.js
+++ src/js/ArrayBufferConverter.js
@@ -1,6 +1,6 @@
 export default class ArrayBufferConverter {
-  constructor() {
-    this.buffer = null;
+  constructor(buffer) {
+    this.buffer = buffer;
   }
 
   load(buffer) {
@@ -9,10 +9,6 @@ export default class ArrayBufferConverter {
 
   toString() {
     const bufferView = new Uint8Array(this.buffer);
-    const output = [];
-    for (let i = 0; i < bufferView.length; i += 1) {
-      output[i] = String.fromCharCode(bufferView[i]);
-    }
-    return output.join('');
+    return [...bufferView].map(char => String.fromCharCode(char)).join('');
   }
 }
